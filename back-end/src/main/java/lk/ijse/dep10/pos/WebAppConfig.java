b5adeaa4074972327b38252864dbebeb87148c96
Ranjith Suranga <d.m.ranjith.sura@gmail.com>
2023-06-12 13:00:25 +0530

    Handle data validation in CustomerController

--- back-end/src/main/java/lk/ijse/dep10/pos/WebAppConfig.java
+++ back-end/src/main/java/lk/ijse/dep10/pos/WebAppConfig.java
@@ -5,6 +5,8 @@ import lk.ijse.dep10.pos.api.ItemController;
 import lk.ijse.dep10.pos.api.OrderController;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
+import org.springframework.validation.beanvalidation.MethodValidationPostProcessor;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @Configuration
