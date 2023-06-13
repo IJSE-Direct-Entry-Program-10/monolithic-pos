9bb9af3b4f01da8f4dd1f0e8eced46aad908d609
Ranjith Suranga <d.m.ranjith.sura@gmail.com>
2023-06-13 17:10:07 +0530

    Finalize Swagger Integration

--- back-end/src/main/java/lk/ijse/dep10/pos/WebAppConfig.java
+++ back-end/src/main/java/lk/ijse/dep10/pos/WebAppConfig.java
@@ -10,8 +10,10 @@ import org.springframework.validation.beanvalidation.MethodValidationPostProcess
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -28,6 +30,16 @@ public class WebAppConfig implements WebMvcConfigurer {
                 .select()
                 .apis(RequestHandlerSelectors.any())
                 .paths(PathSelectors.any())
+                .build()
+                .apiInfo(apiInfo());
+    }
+
+    private ApiInfo apiInfo(){
+        return new ApiInfoBuilder()
+                .title("POS System REST API Documentation")
+                .description("Welcome to POS System REST API Documentation")
+                .version("v1")
+                .license("Copyright (c) 2023 DEP-10. All Rights Reserved.")
                 .build();
     }
 
