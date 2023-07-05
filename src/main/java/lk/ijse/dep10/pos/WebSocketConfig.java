c31ef719f1013c7af5203e7b676ab1dcd3f044c9
Ranjith Suranga <d.m.ranjith.sura@gmail.com>
2023-07-05 06:18:31 +0530

    Finalize the Hibernate Integration

--- src/main/java/lk/ijse/dep10/pos/WebSocketConfig.java
+++ src/main/java/lk/ijse/dep10/pos/WebSocketConfig.java
@@ -1,11 +1,16 @@
 package lk.ijse.dep10.pos;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lk.ijse.dep10.pos.api.CustomerWSHandler;
+import lk.ijse.dep10.pos.business.custom.CustomerBO;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
 import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
 
+@EnableWebSocket
 @Configuration
 public class WebSocketConfig implements WebSocketConfigurer {
 
@@ -20,4 +25,8 @@ public class WebSocketConfig implements WebSocketConfigurer {
         return new CustomerWSHandler();
     }
 
+    @Bean
+    public ObjectMapper objectMapper() {
+        return new ObjectMapper();
+    }
 }
